9224f04ea0173e0416bb4253570dd34ad68d3c1e
Keanna Salter <keannasalter91@gmail.com>
2020-02-12 03:34:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,4 @@
-class Shoe
-  def intialize(brand)
-    @brand = brand
-  end
-  
-  attr_reader: brand
-  attr_accessor : color, size, material, condition
-  
-  def cobble(do)
-    self.condition = "new"
-    puts "Your shoe is as good is new"
-  end
-  
-end
+
 
 class Book
   attr_accessor :author, :page_count, :genre
